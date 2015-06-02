3fb9939e4b1b6c6605958a8090f51cf722eddf27
viniciuschiele <vinicius.chiele@gmail.com>
2015-06-03 00:14:47 +0100

    Updated example

--- examples/advanced.py
+++ examples/advanced.py
@@ -15,11 +15,11 @@ class Config(object):
         }
     ]
 
-    APSCHEDULER_JOBSTORES = {
+    SCHEDULER_JOBSTORES = {
         'default': SQLAlchemyJobStore(url='sqlite://')
     }
 
-    APSCHEDULER_EXECUTORS = {
+    SCHEDULER_EXECUTORS = {
         'default': {'type': 'threadpool', 'max_workers': 20}
     }
 
