9888df6d14226cc81f329d4f9938a5a915f95d7a
viniciuschiele <vinicius.chiele@gmail.com>
2015-11-16 08:52:46 +0000

    Updated APScheduler to 3.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='vinicius.chiele@gmail.com',
     description='Adds APScheduler support to Flask',
     keywords=['apscheduler', 'scheduler', 'scheduling', 'cron'],
-    install_requires=['flask>=0.10.1', 'apscheduler==3.0.3'],
+    install_requires=['flask>=0.10.1', 'apscheduler==3.0.4'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
