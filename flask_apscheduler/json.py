e753038de039fd23f0d59bb0094f59fc73efe22b
Vinicius Chiele <vinicius.chiele@gmail.com>
2016-01-12 20:26:13 -0200

    Set a custom JSON Encoder to serialize date class.

--- flask_apscheduler/json.py
+++ flask_apscheduler/json.py
@@ -1,20 +1,11 @@
+import datetime
 import flask
 import json
 
-from datetime import datetime
 from apscheduler.job import Job
 from .utils import job_to_dict
 
-
-class JSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-
-        if isinstance(obj, Job):
-            return job_to_dict(obj)
-
-        return super(JSONEncoder, self).default(obj)
+loads = json.loads
 
 
 def dumps(obj, indent=None):
@@ -26,3 +17,17 @@ def jsonify(data, status=None):
     if flask.current_app.config['JSONIFY_PRETTYPRINT_REGULAR'] and not flask.request.is_xhr:
         indent = 2
     return flask.current_app.response_class(dumps(data, indent=indent), status=status, mimetype='application/json')
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+
+        if isinstance(obj, datetime.date):
+            return obj.isoformat()
+
+        if isinstance(obj, Job):
+            return job_to_dict(obj)
+
+        return super(JSONEncoder, self).default(obj)
