eaf5b17cd1e72fd2944097441d6c72854eef7194
viniciuschiele <vinicius.chiele@gmail.com>
2016-02-16 08:59:35 +0000

    Release 1.3.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-APScheduler',
-    version='1.3.6',
+    version='1.3.7',
     packages=['flask_apscheduler'],
     url='https://github.com/viniciuschiele/flask-apscheduler',
     license='Apache 2.0',
