ba016fdf2b8a6db40fa1235ac50d36f6fef595ab
Colin MacKenzie <colin@flyingeinstein.com>
2021-11-15 16:15:15 -0500

    fixed pip install error trying to install LICENSE to /usr (or given prefix)

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
     long_description_content_type='text/x-rst',
     keywords=['apscheduler', 'scheduler', 'scheduling', 'cron'],
     install_requires=['flask>=0.10.1', 'apscheduler>=3.2.0,<4.0.0', 'python-dateutil>=2.4.2'],
-    data_files=[('', ['LICENSE'])],
+    package_data={'Flask-APScheduler': ['LICENSE']},
+    include_package_data=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
